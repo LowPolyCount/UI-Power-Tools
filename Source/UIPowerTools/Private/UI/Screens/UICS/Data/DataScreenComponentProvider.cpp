50d86407c995206c9f25b5b962911f7564d0578d
lowpolycount <lowpolycount@noreply.codeberg.org>
2025-08-14 10:18:49 -0400

    Add Blueprint Support for building DataScreenComponentProviders

--- Source/UIPowerTools/Private/UI/Screens/UICS/Data/DataScreenComponentProvider.cpp
+++ Source/UIPowerTools/Private/UI/Screens/UICS/Data/DataScreenComponentProvider.cpp
@@ -3,3 +3,27 @@
 
 #include "UI/Screens/UICS/Data/DataScreenComponentProvider.h"
 
+void UDataScreenComponentProvider::Setup()
+{
+	if (GetClass()->IsFunctionImplementedInScript(FName(L"BP_Setup")))
+	{
+		BP_Setup();
+	}
+}
+
+// retrieve entries and place them in the given TArray
+void UDataScreenComponentProvider::RetrieveEntries(UDataScreenComponent* Component, TArray<UObject*>& RetrievedEntries)
+{
+	if (GetClass()->IsFunctionImplementedInScript(FName(L"BP_RetrieveEntries")))
+	{
+		BP_RetrieveEntries(Component, RetrievedEntries);
+	}
+}
+
+void UDataScreenComponentProvider::Teardown()
+{ 
+	if (GetClass()->IsFunctionImplementedInScript(FName(L"BP_Teardown")))
+	{
+		BP_Teardown();
+	}
+}
--- Source/UIPowerTools/Public/UI/Screens/UICS/Data/DataScreenComponentProvider.h
+++ Source/UIPowerTools/Public/UI/Screens/UICS/Data/DataScreenComponentProvider.h
@@ -13,9 +13,25 @@ class UIPOWERTOOLS_API UDataScreenComponentProvider : public UObject
 {
 	GENERATED_BODY()
 public:
-	virtual void Setup() {}
+	// run before Retrieve Entries to handle any setup needed
+	virtual void Setup();
 
-	// retrieve entries and place them in 
-	virtual void RetrieveEntries(UDataScreenComponent* Component, TArray<UObject*>& RetrievedEntries) PURE_VIRTUAL(UDataScreenComponentProvider::RetrieveEntries);
-	virtual void Teardown() {}
+	// retrieve entries and place them in the given TArray
+	virtual void RetrieveEntries(UDataScreenComponent* Component, TArray<UObject*>& RetrievedEntries);
+
+	// run after RetrieveEntries to handle any teardown needed
+	virtual void Teardown();
+
+protected:
+	// run before Retrieve Entries to handle any setup needed
+	UFUNCTION(BlueprintImplementableEvent, Meta=(DisplayName="Setup"))
+	void BP_Setup();
+
+	// retrieve entries and place them in the given TArray
+	UFUNCTION(BlueprintImplementableEvent, Meta=(DisplayName="RetrieveEntries"))
+	void BP_RetrieveEntries(UDataScreenComponent* Component, TArray<UObject*>& RetrievedEntries);
+
+	// run after RetrieveEntries to handle any teardown needed
+	UFUNCTION(BlueprintImplementableEvent, Meta=(DisplayName="Teardown"))
+	void BP_Teardown();
 };
