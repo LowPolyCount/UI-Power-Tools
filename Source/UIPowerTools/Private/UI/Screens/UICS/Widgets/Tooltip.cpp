b28e307edbab4957c1373c26a2efa9b9cf33aa9f
lowpolycount <lowpolycount@noreply.codeberg.org>
2025-08-23 20:38:23 -0400

    Finish tooltip implementation

--- Source/UIPowerTools/Private/UI/Screens/UICS/Widgets/Tooltip.cpp
+++ Source/UIPowerTools/Private/UI/Screens/UICS/Widgets/Tooltip.cpp
@@ -2,4 +2,124 @@
 
 
 #include "UI/Screens/UICS/Widgets/Tooltip.h"
+#include "UI/Screens/UICS/ViewScreenComponent.h"
 
+
+void UTooltip::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	for (const FViewComponentSelector& ViewSelector : ViewsToListenTo)
+	{
+		StartListeningToView(ViewSelector);
+	}
+}
+
+void UTooltip::NativeDestruct()
+{
+	for (const FViewComponentSelector& ViewSelector : ViewsToListenTo)
+	{
+		StopListeningToView(ViewSelector);
+	}
+
+	Super::NativeDestruct();
+}
+
+void UTooltip::StartListeningToView(const FViewComponentSelector& ViewSelector)
+{
+	if (ViewSelector.IsValid())
+	{
+		if (UViewScreenComponent* ViewComponent = GetScreenComponentFromSelector<UViewScreenComponent>(ViewSelector))
+		{
+			ViewComponent->OnAction.AddUniqueDynamic(this, &UTooltip::HandleOnAction);
+			ViewComponent->OnFocusChange.AddUniqueDynamic(this, &UTooltip::HandleOnFocusChange);
+			ViewComponent->OnSelectionChange.AddUniqueDynamic(this, &UTooltip::HandleOnSelectionChange);
+			ViewComponent->OnHoverChange.AddUniqueDynamic(this, &UTooltip::HandleOnHoverChange);
+		}
+	}
+}
+
+void UTooltip::StopListeningToView(const FViewComponentSelector& ViewSelector)
+{
+	if (ViewSelector.IsValid())
+	{
+		if (UViewScreenComponent* ViewComponent = GetScreenComponentFromSelector<UViewScreenComponent>(ViewSelector))
+		{
+			ViewComponent->OnAction.RemoveDynamic(this, &UTooltip::HandleOnAction);
+			ViewComponent->OnFocusChange.RemoveDynamic(this, &UTooltip::HandleOnFocusChange);
+			ViewComponent->OnSelectionChange.RemoveDynamic(this, &UTooltip::HandleOnSelectionChange);
+			ViewComponent->OnHoverChange.RemoveDynamic(this, &UTooltip::HandleOnHoverChange);
+		}
+	}
+}
+
+void UTooltip::HandleOnAction(UViewScreenComponent* Component, const TScriptInterface<IEntryWidgetInterface>& Widget)
+{
+	BP_HandleOnAction(Component, Widget);
+}
+
+void UTooltip::HandleOnSelectionChange(UViewScreenComponent* Component, const TScriptInterface<IEntryWidgetInterface>& Widget, bool bGained)
+{
+	BP_HandleOnSelectionChange(Component, Widget, bGained);
+}
+
+void UTooltip::HandleOnFocusChange(UViewScreenComponent* Component, const TScriptInterface<IEntryWidgetInterface>& Widget, bool bGained)
+{
+	if (bGained && bShowOnFocusGain)
+	{
+		ShowOrHideTooltip(Widget, bGained);
+	}
+	else if (!bGained && bHideOnFocusLoss)
+	{
+		ShowOrHideTooltip(Widget, bGained);
+	}
+
+	BP_HandleOnFocusChange(Component, Widget, bGained);
+}
+
+void UTooltip::HandleOnHoverChange(UViewScreenComponent* Component, const TScriptInterface<IEntryWidgetInterface>& Widget, bool bGained)
+{
+	if (bGained && bShowOnHoverGain)
+	{
+		ShowOrHideTooltip(Widget, bGained);
+	}
+	else if (!bGained && bHideOnHoverLoss)
+	{
+		ShowOrHideTooltip(Widget, bGained);
+	}
+
+	BP_HandleOnHoverChange(Component, Widget, bGained);
+}
+
+void UTooltip::Populate(UObject* Data)
+{
+	if (ObservedEntryData)
+	{
+		Reset();
+	}
+	SetObservedEntryData(Data);
+	BP_Populate(Data);
+}
+
+void UTooltip::Reset()
+{
+	BP_Reset();
+}
+
+void UTooltip::ShowOrHideTooltip(const TScriptInterface<IEntryWidgetInterface>& Widget, const bool bGained)
+{
+	SetVisibility((bGained) ? ShowRule : HideRule);
+	if (bGained)
+	{
+		Populate(Widget->Execute_GetEntryData(Widget.GetObject()));
+	}
+	else
+	{
+		Reset();
+	}
+}
+
+void UTooltip::SetObservedEntryData(UObject* InData)
+{
+	ObservedEntryData = InData;
+}
--- Source/UIPowerTools/Public/UI/Screens/UICS/Widgets/Tooltip.h
+++ Source/UIPowerTools/Public/UI/Screens/UICS/Widgets/Tooltip.h
@@ -4,14 +4,109 @@
 
 #include "CoreMinimal.h"
 #include "CommonActivatableWidget.h"
+#include "UI/Screens/Tools/ComponentSelector.h"
+#include "UI/Screens/UICS/IUICSAccessor.h"
 #include "Tooltip.generated.h"
+
+class UViewScreenComponent;
+class IEntryWidgetInterface;
 
 /**
  * Listens to provided Screen Components and can display detailed information about a hovered or focused entry
  */
 UCLASS()
-class UIPOWERTOOLS_API UTooltip : public UCommonActivatableWidget
+class UIPOWERTOOLS_API UTooltip : public UCommonActivatableWidget, public IUICSAccessor
 {
 	GENERATED_BODY()
+protected:
+	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
+
+public:
+	// get all view selectors
+	UFUNCTION(BlueprintCallable)
+	const TArray<FViewComponentSelector>& GetAllViewSelectors() const {return ViewsToListenTo;}
+
+	// populate the tooltip with the given data
+	UFUNCTION(BlueprintCallable)
+	void Populate(UObject* Data);
+
+	// reset the tooltip
+	UFUNCTION(BlueprintCallable)
+	void Reset();
+
+protected:
+
+	// Populate the widget. The given EntryData can also be retrieved from ObservedEntryData
+	UFUNCTION(BlueprintImplementableEvent, Meta=(DisplayName="Populate"))
+	void BP_Populate(UObject* EntryData);
+
+	// Reset the tooltip
+	UFUNCTION(BlueprintImplementableEvent, Meta = (DisplayName = "Reset"))
+	void BP_Reset();
+
+	// list out BP Events that can happen to an entry widget that is being observed  
 	
+	// handle the case where the observed entry has an input action happen
+	UFUNCTION(BlueprintImplementableEvent, Meta=(DisplayName="HandleOnAction"))
+	void BP_HandleOnAction(UViewScreenComponent* Component, const TScriptInterface<IEntryWidgetInterface>& Widget);
+
+	// handle the case where the observed entry has it's selection property changed
+	UFUNCTION(BlueprintImplementableEvent, Meta=(DisplayName="HandleOnSelectionChange"))
+	void BP_HandleOnSelectionChange(UViewScreenComponent* Component, const TScriptInterface<IEntryWidgetInterface>& Widget, bool bGained);
+
+	// handle the case where the observed entry has it's focus property changed
+	UFUNCTION(BlueprintImplementableEvent, Meta=(DisplayName="HandleOnFocusChange"))
+	void BP_HandleOnFocusChange(UViewScreenComponent* Component, const TScriptInterface<IEntryWidgetInterface>& Widget, bool bGained);
+
+	// handle the case where the observed entry has it's hover property changed
+	UFUNCTION(BlueprintImplementableEvent, Meta=(DisplayName="HandleOnHoverChange"))
+	void BP_HandleOnHoverChange(UViewScreenComponent* Component, const TScriptInterface<IEntryWidgetInterface>& Widget, bool bGained);
+
+	UFUNCTION()
+	virtual void HandleOnAction(UViewScreenComponent* Component, const TScriptInterface<IEntryWidgetInterface>& Widget);
+	UFUNCTION()
+	virtual void HandleOnSelectionChange(UViewScreenComponent* Component, const TScriptInterface<IEntryWidgetInterface>& Widget, bool bGained);
+	UFUNCTION()
+	virtual void HandleOnFocusChange(UViewScreenComponent* Component, const TScriptInterface<IEntryWidgetInterface>& Widget, bool bGained);
+	UFUNCTION()
+	virtual void HandleOnHoverChange(UViewScreenComponent* Component, const TScriptInterface<IEntryWidgetInterface>& Widget, bool bGained);
+
+	virtual void StartListeningToView(const FViewComponentSelector& View);
+	virtual void StopListeningToView(const FViewComponentSelector& View);
+
+	virtual void ShowOrHideTooltip(const TScriptInterface<IEntryWidgetInterface>& Widget, const bool bGained);
+	virtual void SetObservedEntryData(UObject* InData);
+
+	// the view screen components that we will listen to events from
+	UPROPERTY(EditAnywhere)
+	TArray<FViewComponentSelector> ViewsToListenTo;
+
+	// show tooltip when gaining hover
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bShowOnFocusGain = true;
+
+	// hide tooltip when losing hover
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bHideOnFocusLoss = true;
+
+	// show tooltip when gaining hover
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bShowOnHoverGain = true;
+
+	// hide tooltip when losing hover
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bHideOnHoverLoss = true;
+
+
+	// visibility rule to use when showing tooltip
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	ESlateVisibility ShowRule = ESlateVisibility::SelfHitTestInvisible;
+
+	// visibility rule to use when hiding tooltip
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	ESlateVisibility HideRule = ESlateVisibility::Hidden;
+
+	UPROPERTY(BlueprintReadWrite)
+	TObjectPtr<UObject> ObservedEntryData;
 };
