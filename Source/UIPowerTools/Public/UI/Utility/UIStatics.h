71f376cb09683e345136ac4b33d3132fc11e4a66
lowpolycount <lowpolycount@noreply.codeberg.org>
2025-10-13 18:59:30 -0400

    Add UI Statics

---
+++ Source/UIPowerTools/Public/UI/Utility/UIStatics.h
@@ -0,0 +1,16 @@
+// Copyright 2025 Joel Gonzales
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "UIStatics.generated.h"
+
+/**
+ * A Place to put UI static functions
+ */
+UCLASS()
+class UIPOWERTOOLS_API UUIStatics : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+};
