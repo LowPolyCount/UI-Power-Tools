59c7e85e512fb40b56431a62e4d20f5e367a3682
lowpolycount <lowpolycount@noreply.codeberg.org>
2026-02-11 23:30:13 -0500

    Update ActionScreenComponentProvider.h

--- Source/UIPowerTools/Public/UI/Screens/UICS/Transaction/ActionScreenComponentProvider.h
+++ Source/UIPowerTools/Public/UI/Screens/UICS/Transaction/ActionScreenComponentProvider.h
@@ -18,11 +18,12 @@ class UIPOWERTOOLS_API UActionScreenComponentProvider : public UScreenComponentW
 public:
 	// Do we have all the information required to execute the transaction?
 	UFUNCTION(BlueprintNativeEvent, Category = ActionScreenComponent)
-	bool CanExecuteAction(UActionScreenComponent* Component, UObject* Entry);
+	bool CanExecuteAction(UActionScreenComponent* Component, UObject* ViewWidgetData);
 
 	// execute the transaction
+	// WidgetEntry - this is data coming from a view widget. 
 	UFUNCTION(BlueprintNativeEvent, Category = ActionScreenComponent)
-	ETransactionResult ExecuteAction(UActionScreenComponent* Component, UObject* Entry);
+	ETransactionResult ExecuteAction(UActionScreenComponent* Component, UObject* ViewWidgetData);
 
 protected:
 	virtual ETransactionResult ExecuteAction_Implementation(UActionScreenComponent* Component, UObject* Entry);
