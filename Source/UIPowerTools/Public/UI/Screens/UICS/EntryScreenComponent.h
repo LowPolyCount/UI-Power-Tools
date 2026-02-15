e75ec397814c773c0e90b1e1fa291bcc3fa173ee
lowpolycount <lowpolycount@noreply.codeberg.org>
2026-02-15 18:48:43 -0500

    OnEntryChange added to blueprint bindings

--- Source/UIPowerTools/Public/UI/Screens/UICS/EntryScreenComponent.h
+++ Source/UIPowerTools/Public/UI/Screens/UICS/EntryScreenComponent.h
@@ -7,14 +7,27 @@
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FEntryDelegate, UEntryScreenComponent*, Component, UObject*, OldEntry, UObject*, NewEntry);
 
+// define all bindable events in a struct so that in editor, it will be it's own category
+USTRUCT()
+struct UIPOWERTOOLS_API FBindableEntryEvents
+{
+	GENERATED_BODY()
+
+	// we called ExecuteAction() - what was the result?
+	UPROPERTY(EditAnywhere, Category = "Events", Meta=(FunctionReference, AllowFunctionLibraries, PrototypeFunction="/Script/UIPowerTools.EntryScreenComponent.Binding_OnEntryChange", DefaultBindingName="EntryChange", DisplayName = "OnEntryChange"))
+	FMemberReference  Bind_OnEntryChange;
+};
+
 // contains an entry (one piece of data) that can be held and accessed by other components
 UCLASS(BlueprintType)
 class UIPOWERTOOLS_API UEntryScreenComponent : public UScreenComponent
 {
 	GENERATED_BODY()
 public:
-
+	// called when the entry we hold changes
+	UPROPERTY(BlueprintAssignable, Category=EntryScreenComponent)
 	FEntryDelegate OnEntryChange;
+
 	// get the entry
 	UFUNCTION(BlueprintCallable, Category = EntryScreenComponent)
 	UObject* GetEntry() const { return HeldEntry; }
@@ -37,4 +50,13 @@ protected:
 
 	UPROPERTY()
 	TObjectPtr<UObject> HeldEntry;
+
+	UPROPERTY(EditAnywhere, Category = EntryScreenComponent, Meta=(DisplayName="Events"));
+	FBindableEntryEvents BindableEvents;
+
+	// BEGIN FMember References that allow you to bind events to functions in editor
+#if WITH_EDITOR
+	UFUNCTION(BlueprintInternalUseOnly)
+	void Binding_OnEntryChange(UEntryScreenComponent* Component, UObject* OldEntry, UObject* NewEntry) {}
+#endif // WITH_EDITOR
 };
